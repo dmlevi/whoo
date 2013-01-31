0fb9a15542a79768ac9229b709d5f1738b13540f
Dave Levine <apprentice5@steve-fts.local>
2013-01-31 16:01:25 -0500

    auto credit card detection added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,63 @@
 $(document).ready(function(){
 
+  jQuery.validator.setDefaults({
+    onkeyup: false
+  });
+
+  function getCreditCardType() {
+
+    var accountNumber = document.getElementById("card_number").value;
+
+    console.log(accountNumber);
+
+    if (/^5[1-5]/.test(accountNumber)) {
+      document.getElementById("mastercard").setAttribute("checked");
+    }
+
+    else if (/^4/.test(accountNumber)) {
+      document.getElementById("visa").setAttribute("checked");
+    }
+
+    else if (/^3[47]/.test(accountNumber)) {
+
+      document.getElementById("amex").setAttribute("checked");
+    }
+
+     else if (/^6/.test(accountNumber)) {
+
+      document.getElementById("discover").setAttribute("checked");
+    }
+
+  }
+
+  document.getElementById("card_number").addEventListener('keyup',getCreditCardType);
+
+
+jQuery.validator.addMethod("password", function( value, element ) {
+    var result = this.optional(element) || value.length >= 6 && /\d/.test(value) && /[a-z]/i.test(value);
+    if (!result) {
+      element.value = "";
+      var validator = this;
+      setTimeout(function() {
+        validator.blockFocusCleanup = true;
+        element.focus();
+        validator.blockFocusCleanup = false;
+      }, 1);
+    }
+    return result;
+  }, "Your password must be at least 6 characters long and contain at least one number and one character.");
+
 	$('#form').validate({
 		rules: {
 			card_number: {
-				required: true
+				required: true,
+        creditcard: true
 			},
 			security_code: {
-				required: true
+				required: true,
+        digits: true,
+        minlength: 3,
+        maxlength: 4
 			},
 			portfolio_title: {
 				required: true,
@@ -23,7 +74,8 @@ $(document).ready(function(){
 				email: true
 			},
 			password: {
-				required: true,
+        required: true,
+        password: true
 			},
       month: {
         required: true
