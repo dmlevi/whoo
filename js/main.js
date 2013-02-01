e9b6c3ad5beb8880e01382d8ab5dfdd83af1fdad
Dave Levine <apprentice5@steve-fts.local>
2013-02-01 11:14:34 -0500

    added myers reset and fixed css errors

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
           "visa": /^4/,
           "mastercard": /^5[1-5]/,
           "amex": /^3[47]/,
-          "discover":/^6./
+          "discover":/^6/
       }
 
   function getCreditCardType() {
@@ -21,48 +21,8 @@
             setCardStatus(card, true)
           };
       }
-
   }
 
   document.getElementById("card_number").addEventListener('keyup',getCreditCardType);
 
 })(); // end anonymous function and call it
-
-
-// for each cardType in cardObject,
-//   get element with cardType as ID and setAttribute unchecked <- a method with 2 arguments: cardtype and check/uncheck
-
-
-// var cards = {
-
-//     x: value1,
-//     y: {key1: a,
-//          key2: b
-//        }
-//     z: function(){}
-
-// };
-
-// cards.x <- value1
-// cards.y <- {key1: a, key2: b}
-// cards.y.key1 <- a
-// cards.y.key2 <- b
-// cards.z <- "function"
-// cards.z() <- runs that function
-
-// array = [a,b,c,d]
-// object = {0:a, 1:b, 2:c, 3:d}
-
-// array[0]
-// object[0]
-
-// console.log(for i in array return i) <- a,b,c,d
-// console.log(for i in object return i) <- whatever the fuck order, but always all of them
-
-// cardReader.[key].test(accountNum) <- tells you whether
-
-
-// for each cardType in cardObject
-//   get cardRegEx and test against accountNumber
-//   if cardRegEx is a hit,
-//     get element with cardtype as ID and setAttribute checked
